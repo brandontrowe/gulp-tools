8ab675fb5d09afa5befddd8d1824108fa32d97dd
brandon rowe <brandon.rowe@helloworld.com>
2016-10-21 14:43:25 -0400

    add instructions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gulp-tools
 Misc scripts to handle repetitive functions
+
+## clean-file-name
+Removes common unwanted characters from filenames and resaves the file
--- clean-file-name/gulpfile.js
+++ clean-file-name/gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 
 const settings = {
-    src: '/',
-    dest: 'clean'
+    srcFolder: './', // source folder of the files to be cleaned
+    destFolder: 'clean' // relative to srcFolder; set to '' to have the files saved in the same folder
 }
 
-function cleanName(filename) {
+/**
+ * Removes unwanted characters from the provided string
+ * @param {string} filename - String to be cleaned.
+ * @returns {string}
+ */
+const getCleanFilename = function(filename) {
     return filename
         .replace(/ /g, '-') // remove spaces
         .replace(/,/g, '-') // remove commas
@@ -17,13 +22,13 @@ function cleanName(filename) {
 }
 
 gulp.task('clean', () => {
-    gulp.src(settings.src + "*.*")
+    gulp.src(settings.srcFolder + "*.*")
         .pipe(rename(function (path) {
             console.log('org-name:', path.basename + path.extname);
-            path.basename = cleanName(path.basename);
+            path.basename = getCleanFilename(path.basename) + '';
             console.log('new-name:', path.basename + path.extname, '\n');
         }))
-        .pipe(gulp.dest(settings.src + settings.dest));
+        .pipe(gulp.dest(settings.srcFolder + settings.destFolder));
 });
 
 gulp.task('default', ['clean'], function () {});
